d2995f3af796e638fe09e6331da54660e6f1aec7
Steven Lasch <91107394+s-lasch@users.noreply.github.com>
2023-05-26 03:41:43 -0400

    Update copy-code-button.js

--- js/highlightjs/copy-code-button.js
+++ js/highlightjs/copy-code-button.js
@@ -1,18 +1,52 @@
+// function addCopyButtons(clipboard) {
+//     document.querySelectorAll('pre > code').forEach(function (codeBlock) {
+//         var button = document.createElement('button');
+//         button.className = 'copy-code-button';
+//         button.type = 'button';
+//         button.innerText = 'Copy';
+//         button.addEventListener('click', function () {
+//             clipboard.writeText(codeBlock.innerText).then(function () {
+//                 button.blur();
+//                 button.innerText = 'Copied!';
+//                 setTimeout(function () {button.innerText = 'Copy';}, 2000);
+//             }, function (error) {button.innerText = 'Error';});
+//         });
+//         var pre = codeBlock.parentNode;
+//         pre.parentNode.insertBefore(button, pre);
+//     });
+// }
+
+// addCopyButtons(navigator.clipboard);
 function addCopyButtons(clipboard) {
     document.querySelectorAll('pre > code').forEach(function (codeBlock) {
         var button = document.createElement('button');
         button.className = 'copy-code-button';
         button.type = 'button';
         button.innerText = 'Copy';
+
         button.addEventListener('click', function () {
             clipboard.writeText(codeBlock.innerText).then(function () {
+                /* Chrome doesn't seem to blur automatically,
+                   leaving the button in a focused state. */
                 button.blur();
+
                 button.innerText = 'Copied!';
-                setTimeout(function () {button.innerText = 'Copy';}, 2000);
-            }, function (error) {button.innerText = 'Error';});
+
+                setTimeout(function () {
+                    button.innerText = 'Copy';
+                }, 2000);
+            }, function (error) {
+                button.innerText = 'Error';
+            });
         });
+
         var pre = codeBlock.parentNode;
-        pre.parentNode.insertBefore(button, pre);
+        if (pre.parentNode.classList.contains('highlight')) {
+            var highlight = pre.parentNode;
+            highlight.parentNode.insertBefore(button, highlight);
+        } else {
+            pre.parentNode.insertBefore(button, pre);
+        }
     });
 }
 
