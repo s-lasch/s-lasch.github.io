2ce6f9660390273630133205499b298a3baf55bf
Steven Lasch <91107394+s-lasch@users.noreply.github.com>
2023-05-26 03:13:42 -0400

    Create copy-code-button.js

---
+++ js/highlightjs/copy-code-button.js
@@ -0,0 +1,19 @@
+function addCopyButtons(clipboard) {
+    document.querySelectorAll('pre > code').forEach(function (codeBlock) {
+        var button = document.createElement('button');
+        button.className = 'copy-code-button';
+        button.type = 'button';
+        button.innerText = 'Copy';
+        button.addEventListener('click', function () {
+            clipboard.writeText(codeBlock.innerText).then(function () {
+                button.blur();
+                button.innerText = 'Copied!';
+                setTimeout(function () {button.innerText = 'Copy';}, 2000);
+            }, function (error) {button.innerText = 'Error';});
+        });
+        var pre = codeBlock.parentNode;
+        pre.parentNode.insertBefore(button, pre);
+    });
+}
+
+addCopyButtons(navigator.clipboard);
